23cc0ffe60cddf3d75a4d8ceee66cd8de6c303e5
Sarah Wong <sarah.wong.0x00@gmail.com>
2014-08-17 13:15:41 -0400

    Move F0_params to stack

--- get_f0.cpp
+++ get_f0.cpp
@@ -68,53 +68,51 @@ int main_sw_tmp(int ac, char *av[])
   int done;
   long buff_size, actsize;
   double sf, output_starts, frame_rate;
-  F0_params *par;
   float *f0p, *vuvp, *rms_speech, *acpkp;
   int i, vecsize;
   long sdstep = 0;
 
-  par = (F0_params *) malloc(sizeof(F0_params));
-  par->cand_thresh = 0.3;
-  par->lag_weight = 0.3;
-  par->freq_weight = 0.02;
-  par->trans_cost = 0.005;
-  par->trans_amp = 0.5;
-  par->trans_spec = 0.5;
-  par->voice_bias = 0.0;
-  par->double_cost = 0.35;
-  par->min_f0 = 50;
-  par->max_f0 = 550;
-  par->frame_step = 0.01;
-  par->wind_dur = 0.0075;
-  par->n_cands = 20;
-  par->mean_f0 = 200;     /* unused */
-  par->mean_f0_weight = 0.0;  /* unused */
-  par->conditioning = 0;    /*unused */
-
+  F0_params par;
+  par.cand_thresh = 0.3;
+  par.lag_weight = 0.3;
+  par.freq_weight = 0.02;
+  par.trans_cost = 0.005;
+  par.trans_amp = 0.5;
+  par.trans_spec = 0.5;
+  par.voice_bias = 0.0;
+  par.double_cost = 0.35;
+  par.min_f0 = 50;
+  par.max_f0 = 550;
+  par.frame_step = 0.01;
+  par.wind_dur = 0.0075;
+  par.n_cands = 20;
+  par.mean_f0 = 200;     /* unused */
+  par.mean_f0_weight = 0.0;  /* unused */
+  par.conditioning = 0;    /*unused */
 
 #define SW_CUSTOMIZABLE(x) //TODO(sw)
   SW_CUSTOMIZABLE(debug_level);
 
-  SW_CUSTOMIZABLE(par->frame_step);
-  SW_CUSTOMIZABLE(par->cand_thresh);
-  SW_CUSTOMIZABLE(par->lag_weight);
-  SW_CUSTOMIZABLE(par->freq_weight);
-  SW_CUSTOMIZABLE(par->trans_cost);
-  SW_CUSTOMIZABLE(par->trans_amp);
-  SW_CUSTOMIZABLE(par->trans_spec);
-  SW_CUSTOMIZABLE(par->voice_bias);
-  SW_CUSTOMIZABLE(par->double_cost);
-  SW_CUSTOMIZABLE(par->min_f0);
-  SW_CUSTOMIZABLE(par->max_f0);
-  SW_CUSTOMIZABLE(par->wind_dur);
-  SW_CUSTOMIZABLE(par->n_cands);
+  SW_CUSTOMIZABLE(par.frame_step);
+  SW_CUSTOMIZABLE(par.cand_thresh);
+  SW_CUSTOMIZABLE(par.lag_weight);
+  SW_CUSTOMIZABLE(par.freq_weight);
+  SW_CUSTOMIZABLE(par.trans_cost);
+  SW_CUSTOMIZABLE(par.trans_amp);
+  SW_CUSTOMIZABLE(par.trans_spec);
+  SW_CUSTOMIZABLE(par.voice_bias);
+  SW_CUSTOMIZABLE(par.double_cost);
+  SW_CUSTOMIZABLE(par.min_f0);
+  SW_CUSTOMIZABLE(par.max_f0);
+  SW_CUSTOMIZABLE(par.wind_dur);
+  SW_CUSTOMIZABLE(par.n_cands);
 #undef SW_CUSTOMIZABLE
 
 #define SW_FILE_PARAMS(x, y) //TODO (sw)
   SW_FILE_PARAMS(sf, "sampling frequency");
 #undef SW_FILE_PARAMS
 
-  if(check_f0_params(par, sf)){
+  if(check_f0_params(&par, sf)){
     Fprintf(stderr, "invalid/inconsistent parameters -- exiting.\n");
     exit(1);
   }
@@ -123,15 +121,15 @@ int main_sw_tmp(int ac, char *av[])
     if (total_samps < ((par->frame_step * 2.0) + par->wind_dur) * sf), then
       input range too small*/
 
-  output_starts = par->wind_dur/2.0;
+  output_starts = par.wind_dur/2.0;
   /* Average delay due to loc. of ref. window center. */
-  frame_rate = 1.0 / par->frame_step;
+  frame_rate = 1.0 / par.frame_step;
 
 
   /* Initialize variables in get_f0.c; allocate data structures;
    * determine length and overlap of input frames to read.
    */
-  if (init_dp_f0(sf, par, &buff_size, &sdstep)
+  if (init_dp_f0(sf, &par, &buff_size, &sdstep)
       || buff_size > INT_MAX || sdstep > INT_MAX)
   {
     Fprintf(stderr, "problem in init_dp_f0().\n");
@@ -152,7 +150,7 @@ int main_sw_tmp(int ac, char *av[])
 
     done = (actsize < buff_size);
 
-    if (dp_f0(fdata, (int) actsize, (int) sdstep, sf, par,
+    if (dp_f0(fdata, (int) actsize, (int) sdstep, sf, &par,
 	      &f0p, &vuvp, &rms_speech, &acpkp, &vecsize, done)) {
       Fprintf(stderr, "problem in dp_f0().\n");
       exit(1);
