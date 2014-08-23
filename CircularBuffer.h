b931c633b86d5b32a1a7f1689c514f503567e46b
Sarah Wong <sarah.wong.0x00@gmail.com>
2014-08-23 15:18:53 -0400

    Looping is working

--- CircularBuffer.h
+++ CircularBuffer.h
@@ -3,6 +3,7 @@
 
 #include <cstddef>
 #include <stdexcept>
+#include <cassert>
 
 template <class T>
 class CircularBuffer {
@@ -13,9 +14,17 @@ public:
     iterator& operator++()
     {
       m_ptr++;
+      if (m_ptr == m_cb.m_capacity) {
+        m_ptr = 0;
+        m_oneMoreLoop = false;
+      }
       return *this;
     }
-    bool operator==(const iterator& o) const { return m_ptr == o.m_ptr; }
+    bool operator==(const iterator& o) const
+    {
+      if (m_oneMoreLoop) return false;
+      return m_ptr == o.m_ptr;
+    }
     bool operator!=(const iterator& o) const { return !(*this == o); }
 
   private:
@@ -23,12 +32,16 @@ public:
 
     CircularBuffer& m_cb;
     size_t m_ptr;
+    bool m_oneMoreLoop;
 
-    iterator(CircularBuffer& cb, size_t ptr) : m_cb(cb), m_ptr(ptr) {}
+    iterator(CircularBuffer& cb, size_t ptr, bool oneMoreLoop)
+        : m_cb(cb), m_ptr(ptr), m_oneMoreLoop(oneMoreLoop)
+    {
+    }
   };
 
   CircularBuffer(std::size_t capacity)
-      : m_capacity(capacity), m_size(0), m_begin(0)
+      : m_capacity(capacity), m_size(0), m_begin(0), m_ptr(0)
   {
     if (capacity == 0) {
       throw std::invalid_argument("Circular buffer cannot be of zero capacity");
@@ -37,12 +50,22 @@ public:
     m_data = new T[capacity];
   }
 
-  iterator begin() { return iterator(*this, m_begin); }
+  iterator begin() { return iterator(*this, m_begin, m_capacity == m_size); }
 
-  iterator end() { return iterator(*this, m_size); }
+  iterator end() { return iterator(*this, m_ptr, false); }
 
-  void push_back(const T& val) {
-    m_data[m_size++] = val;
+  void push_back(const T& val)
+  {
+    if (m_size < m_capacity)
+      m_size++;
+    else
+      m_begin = (m_begin + 1) % m_capacity;
+
+    m_data[m_ptr++] = val;
+
+    if (m_ptr == m_capacity) {
+      m_ptr = 0;
+    }
   }
 
   virtual ~CircularBuffer() { delete m_data; }
@@ -56,7 +79,9 @@ private:
 
   size_t m_capacity;
   size_t m_size;
+
   size_t m_begin;
+  size_t m_ptr;
   T* m_data;
 };
 
--- tests/unittest.cpp
+++ tests/unittest.cpp
@@ -6,7 +6,7 @@ namespace {
 
 class CircularBufferTest : public ::testing::Test {
 protected:
-  CircularBufferTest() : m_cb3(3) { }
+  CircularBufferTest() : m_cb3(3) {}
 
   void SetUp() override {}
 
@@ -14,7 +14,6 @@ protected:
 
   CircularBuffer<int> m_cb3;
 };
-
 }
 
 TEST(CircularBufferZeroTest, TestThrow)
@@ -34,17 +33,43 @@ TEST_F(CircularBufferTest, ReadWithoutLoop)
   m_cb3.push_back(3);
 
   auto iter = m_cb3.begin();
-  ASSERT_NE(iter, m_cb3.end());
+  ASSERT_TRUE(iter != m_cb3.end());
   ASSERT_EQ(*iter, 1);
   ++iter;
 
-  ASSERT_NE(iter, m_cb3.end());
+  ASSERT_TRUE(iter != m_cb3.end());
   ASSERT_EQ(*iter, 2);
   ++iter;
 
-  ASSERT_NE(iter, m_cb3.end());
+  ASSERT_TRUE(iter != m_cb3.end());
   ASSERT_EQ(*iter, 3);
   ++iter;
 
-  ASSERT_EQ(iter, m_cb3.end());
+  ASSERT_TRUE(iter == m_cb3.end())
+      << "Make sure after we read the whole buffer, we stop reading.";
+}
+
+TEST_F(CircularBufferTest, ReadLoop)
+{
+  m_cb3.push_back(1);
+  m_cb3.push_back(2);
+  m_cb3.push_back(3);
+  m_cb3.push_back(4);
+  m_cb3.push_back(5);
+
+  auto iter = m_cb3.begin();
+  ASSERT_TRUE(iter != m_cb3.end());
+  ASSERT_EQ(*iter, 3);
+  ++iter;
+
+  ASSERT_TRUE(iter != m_cb3.end());
+  ASSERT_EQ(*iter, 4);
+  ++iter;
+
+  ASSERT_TRUE(iter != m_cb3.end());
+  ASSERT_EQ(*iter, 5);
+  ++iter;
+
+  ASSERT_TRUE(iter == m_cb3.end())
+      << "Make sure after we read the whole buffer, we stop reading.";
 }
