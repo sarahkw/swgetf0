cfbe48151a27d3f92c0702f71558fc52e98cf04a
Sarah Wong <sarah.wong.0x00@gmail.com>
2014-09-13 15:38:36 -0400

    Vector implementation is obsolete

--- swpitcher.cpp
+++ swpitcher.cpp
@@ -1,84 +1,13 @@
 
-#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <cstdio>
-#include <cstring>
-#include <cassert>
 #include <thread>
-#include <unistd.h>
 
 #include <pulse/simple.h>
 
-#include "GetF0/get_f0.h"
 #include "viewer/viewer.h"
-
-#include "GetF0VectorImpl.h"
 #include "GetF0StreamImpl.h"
 
-namespace {
-
-template <class SourceFormat, class DestFormat>
-struct StaticCaster {
-  DestFormat operator()(SourceFormat from)
-  {
-    return static_cast<DestFormat>(from);
-  }
-};
-
-template <class SourceFormat, class DestFormat>
-int readSamples(std::vector<DestFormat>& output)
-{
-  enum { BUFFER_SIZE = 4096 };
-  SourceFormat buffer[BUFFER_SIZE];
-  size_t readSize;
-
-  do {
-    readSize = std::fread(buffer, sizeof(SourceFormat), BUFFER_SIZE, stdin);
-
-    std::transform(buffer, buffer + readSize, std::back_inserter(output),
-                   StaticCaster<SourceFormat, DestFormat>());
-
-  } while (readSize == BUFFER_SIZE);
-
-  if (std::ferror(stdin) != 0) {
-    std::perror("Error reading samples");
-    return 1;
-  }
-
-  return 0;
-}
-
-}  // namespace anonymous
-
 typedef short DiskSample;
-
-int implementation_vector()
-{
-  using GetF0::GetF0VectorImpl;
-
-  GetF0VectorImpl::SampleVector samples;
-  if (readSamples<DiskSample, GetF0VectorImpl::Sample>(samples) != 0) {
-    return 1;
-  }
-
-  std::cerr << "Read " << samples.size() << " samples." << std::endl;
-
-  GetF0VectorImpl::SampleFrequency freq = 96000;
-  GetF0VectorImpl f0(freq, samples);
-  f0.init();
-  f0.run();
-
-  std::cerr << "Returned " << f0.m_outputVector.size() << " data points."
-            << std::endl;
-  std::cerr << "streamBufferSize " << f0.streamBufferSize() << std::endl;
-  std::cerr << "streamOverlapSize " << f0.streamOverlapSize() << std::endl;
-
-  std::fwrite(f0.m_outputVector.data(), sizeof(GetF0VectorImpl::Sample),
-              f0.m_outputVector.size(), stdout);
-
-  return 0;
-}
 
 int implementation_stream()
 {
@@ -223,10 +152,7 @@ int main(int argc, char* argv[])
 {
   auto implementation = atoi(argv[1]);
 
-  if (implementation == 0) {
-    return implementation_vector();
-  }
-  else if (implementation == 1) {
+  if (implementation == 1) {
     return implementation_stream();
   }
   else if (implementation == 2) {
