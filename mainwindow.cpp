066aab82319b1b0865003aafcff3b93e5ee07693
Sarah Wong <sarah.wong.0x00@gmail.com>
2014-10-18 22:49:44 -0400

    Set up an OpenGL widget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 
-#include <QLabel>
+ViewerWidget::ViewerWidget(QWidget* parent) : QGLWidget(parent) {}
+
+void ViewerWidget::initializeGL() {}
+void ViewerWidget::resizeGL(int w, int h) {}
+void ViewerWidget::paintGL() {}
 
 MainWindow::MainWindow() {
   m_ui.setupUi(this);
 
-  QLabel *label1 = new QLabel("Hi", this);
+  ViewerWidget *vw = new ViewerWidget(this);
 
-  setCentralWidget(label1);
+  setCentralWidget(vw);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,5 +1,20 @@
 #include "ui_mainwindow.h"
 
+#include <QGLWidget>
+
+class ViewerWidget : public QGLWidget {
+  Q_OBJECT
+
+public:
+  ViewerWidget(QWidget* parent);
+
+protected:
+
+  void initializeGL() override;
+  void resizeGL(int w, int h) override;
+  void paintGL() override;
+};
+
 class MainWindow : public QMainWindow {
   Q_OBJECT
 
