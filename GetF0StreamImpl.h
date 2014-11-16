e073ec961baff8d91c58fde121f4ac71503244fd
Sarah Wong <sarah.wong.0x00@gmail.com>
2014-11-16 17:08:59 -0500

    Revert "hmm the name 'swpitcher' sounds better than 'swgetf0'"
    This reverts commit f29a7c3c6dc9533451d0000d1f5c8a2e2c0fd26a.

--- GetF0StreamImpl.h
+++ GetF0StreamImpl.h
@@ -1,9 +1,9 @@
 /*
     Copyright (C) 2014  Sarah Wong
 
-    This file is part of swpitcher.
+    This file is part of swgetf0.
 
-    swpitcher is free software: you can redistribute it and/or modify it
+    swgetf0 is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
