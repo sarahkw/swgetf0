72601eca504a672a39596082e7cebcdef48f810c
Sarah Wong <sarah.wong.0x00@gmail.com>
2014-11-16 17:06:11 -0500

    hmm the name 'swpitcher' sounds better than 'swgetf0'

--- GetF0StreamImpl.cpp
+++ GetF0StreamImpl.cpp
@@ -1,9 +1,9 @@
 /*
     Copyright (C) 2014  Sarah Wong
 
-    This file is part of swgetf0.
+    This file is part of swpitcher.
 
-    swgetf0 is free software: you can redistribute it and/or modify it
+    swpitcher is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
