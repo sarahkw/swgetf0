55308473bf6cac7fbd5afebd6792df380a49ce70
Sarah Wong <sarah.wong.0x00@gmail.com>
2014-11-16 22:30:27 -0500

    validateAudioDevice

--- inputdevice.cpp
+++ inputdevice.cpp
@@ -3,6 +3,7 @@
 
 #include <QAudioInput> // not final
 #include <QDebug> // not final
+#include <QMessageBox>
 
 namespace {
 template <class IndexMapType, class ItemType, class Callable>
@@ -30,6 +31,52 @@ static void insertWithDefault(QComboBox* comboBox, IndexMapType& map,
     comboBox->insertItem(itemPosition++, stringGenerator(item));
   }
 }
+
+bool validateAudioDevice(QString& errorMessage,
+                         QAudioDeviceInfo& audioDeviceInfo)
+{
+#define CONDITION(cond) [](QAudioDeviceInfo & adi) { return cond; }
+
+  struct {
+    std::function<bool(QAudioDeviceInfo& adi)> test;
+    const char* failureMessage;
+  } tests[] = {
+
+        {CONDITION(adi.supportedByteOrders().contains(
+             static_cast<QAudioFormat::Endian>(QSysInfo::ByteOrder))),
+         "Audio device does not support system byte order."},
+
+        {CONDITION(adi.supportedChannelCounts().contains(1)),
+         "Audio device does not support mono input."},
+
+        {CONDITION(adi.supportedCodecs().contains("audio/pcm")),
+         "Audio device does not support PCM codec."},
+
+        {CONDITION(
+             adi.supportedSampleTypes().contains(QAudioFormat::UnSignedInt)),
+         "Audio device does not support unsigned int."},
+
+	/*
+        {CONDITION(false), "Failure test."},
+        {CONDITION(false), "Failure test 2."},
+	*/
+
+    };
+
+#undef CONDITION
+
+  bool good = true;
+  for (auto t : tests) {
+    if (!t.test(audioDeviceInfo)) {
+      if (!good) { errorMessage.append("\n"); }
+      errorMessage.append(t.failureMessage);
+      good = false;
+    }
+  }
+
+  return good;
+}
+
 } // namespace anonymous
 
 InputDevice::InputDevice(QWidget *parent) :
@@ -70,4 +117,9 @@ void InputDevice::on_cmbDevice_currentIndexChanged(int index)
                     preferredAudioFormat.sampleSize(), [](int sampleSize) {
                       return QString::number(sampleSize);
                     });
+
+  QString errorMessage;
+  if (!validateAudioDevice(errorMessage, device)) {
+    QMessageBox::critical(this, tr("Unsupported input device"), errorMessage);
+  }
 }
