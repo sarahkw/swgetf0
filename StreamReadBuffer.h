393c8ef50ea7f70d320ac2b87f26b6504b6adba5
Sarah Wong <sarah.wong.0x00@gmail.com>
2014-11-18 20:28:42 -0500

    progress: some callback thing for StreamReadBuffer

--- StreamReadBuffer.h
+++ StreamReadBuffer.h
@@ -5,6 +5,31 @@
 #ifndef INCLUDED_STREAMREADBUFFER
 #define INCLUDED_STREAMREADBUFFER
 
+#include <cstddef>
+#include <functional>
 
+/*! Read from a source in large chunks, letting another source read
+    from the buffer in smaller chunks.
+
+    This is to appease Qt's audio interface, as multiple small reads
+    eats too much CPU. */
+class StreamReadBuffer {
+public:
+
+  typedef std::function<ssize_t(void*, size_t)> Reader;
+
+  StreamReadBuffer(size_t minimumReadSize, Reader reader)
+      : m_minimumReadSize(minimumReadSize), m_reader(reader)
+  {
+    reader(NULL, 100);
+  }
+
+private:
+
+  size_t m_minimumReadSize;
+
+  Reader m_reader;
+
+};
 
 #endif
--- tests/TestStreamReadBuffer.cpp
+++ tests/TestStreamReadBuffer.cpp
@@ -4,6 +4,8 @@
 
 #include "gtest/gtest.h"
 
+#include <iostream>
+
 #include "../StreamReadBuffer.h"
 
 namespace {
@@ -12,9 +14,20 @@ class TestStreamReadBuffer : public ::testing::Test {
 protected:
   TestStreamReadBuffer() {}
 
-  void SetUp() override {}
+  void SetUp() override {
+    m_srb = new StreamReadBuffer(5, [](void* buffer, size_t size) {
+	std::cout << "Stream read, of size " << size << std::endl;
+	return size;
+    });
+  }
 
   void TearDown() override {}
+
+  StreamReadBuffer *m_srb;
 };
 
+}
+
+TEST_F(TestStreamReadBuffer, Run) {
+  // NOP, just run fixture
 }
